ab01ebcaeea827771f8dc5d1fcbe8fd5cf181a1f
Oblio <oblio.leitch@gmail.com>
2021-06-20 10:03:16 -0400

    migrated to new package

--- src/main/java/com/flier268/autoharvest/Plugin/ModMenu.java
+++ src/main/java/com/flier268/autoharvest/Plugin/ModMenu.java
@@ -1,7 +1,8 @@
 package com.flier268.autoharvest.Plugin;
 
-import io.github.prospector.modmenu.api.ConfigScreenFactory;
-import io.github.prospector.modmenu.api.ModMenuApi;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 
 public class ModMenu implements ModMenuApi {
     @Override
